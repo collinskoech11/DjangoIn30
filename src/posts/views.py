31f4f15cd24167d69bc87be361684246f62156cb
collinskoech11 <collinskoechck34@gmail.com>
2021-05-02 23:03:23 +0300

    added django templates

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# CRUD - create retreive update delete
+
+# list all the posts
+
+def post_list_view(request):
+    post_objects = Post.objects.all()
+    context = {
+        'post_objects': post_objects
+    }
+    return render(request, "index.html", context)
