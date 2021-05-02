5ab30e878b31c5a2b9e38ebe48af4df8617d5445
collinskoech11 <collinskoechck34@gmail.com>
2021-05-02 23:43:20 +0300

    update posts admin

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+
+from .models import Post
 # Create your views here.
 
 # CRUD - create retreive update delete
@@ -11,4 +13,4 @@ def post_list_view(request):
     context = {
         'post_objects': post_objects
     }
-    return render(request, "index.html", context)
+    return render(request, "posts/index.html", context)
